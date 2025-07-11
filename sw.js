f58f072adb42e47a1b2fafa91bddf911056d1241
GLE Elom <elomkhatib@gmail.com>
2025-07-11 10:22:54 -0700

    first

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'gale-nene-v1';
+const urlsToCache = [
+  '/',
+  './about.html',
+  './assets/css/style-starter.css',
+  './assets/images/pexels-mali-64208.jpg',
+  './favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
